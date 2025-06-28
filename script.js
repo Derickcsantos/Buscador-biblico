3757e3e93131571c2c9626b58a7e165e31ca3f62
Derick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-06-27 22:17:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+async function PesquisarDados() {
+    const livro = document.getElementById('livroNome').value;
+    const capitulo = document.getElementById('capituloNumero').value;
+    const verso = document.getElementById('versoNumero').value;
+    const containerInformacoes = document.getElementById('containerInformacoes');
+
+    let url = await fetch(`https://bible-api.com/${livro}+${capitulo}:${verso}?translation=almeida`);
+    let dados = await url.json();
+
+console.log(dados);
+console.log(dados.reference);
+console.log(dados.text);
+console.log(dados.translation_name);
+
+document.getElementById('referencia').innerText = dados.reference;
+document.getElementById('texto').innerText = dados.text;
+document.getElementById('versao').innerText = dados.translation_name;
+containerInformacoes.style.display = "flex";
+}
+
+const btnfecharModal = document.getElementById('fecharModal');
+
+btnfecharModal.addEventListener('click', (e) =>{
+    const containerInformacoes = document.getElementById('containerInformacoes');
+    containerInformacoes.style.display = "none";
+})
